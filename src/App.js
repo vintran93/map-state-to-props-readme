0ee830211fd4f4c0dc016792117a48d01fd58e45
Vincent Tran <vincentuong@yahoo.com>
2022-03-29 21:38:02 -0500

    commit tree d7acdf8976cfdf425136b23b640b1c6d23be1f37

--- src/App.js
+++ src/App.js
@@ -1,21 +1,29 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import { connect } from "react-redux"; 
+
+import "./App.css";
 
 class App extends Component {
-	handleOnClick = () => {
-		this.props.store.dispatch({
-			type: 'INCREASE_COUNT',
-		});
-	}
 
-	render() {
-		return (
-			<div className="App">
-				<button onClick={this.handleOnClick}>Click</button>
-				<p>{this.props.store.getState().clicks}</p>
-			</div>
-		);
-	}
+    handleOnClick = () => {
+        this.props.dispatch({
+          type: "INCREASE_COUNT",
+        });
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <button onClick={this.handleOnClick}>Click</button>
+                <p>{this.props.clicks}</p>
+            </div>
+        );
+    }
 }
 
-export default App;
+
+const mapStateToProps = (state) => {
+    return { clicks: state.clicks };
+};
+
+export default connect(mapStateToProps)(App);
